b8641e15cc7cd902ea7d819a7b671b6b35543856
Aymen Bennour <aymenokay@gmail.com>
2019-01-20 20:44:22 +0100

    add: login component and setup its reactiveForm

--- front-end/src/app/auth/auth.module.ts
+++ front-end/src/app/auth/auth.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthRoutingModule, AuthRoutedComponents } from './auth-routing.module';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     [...AuthRoutedComponents],
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class AuthModule { }
